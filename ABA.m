d8db53b019765df9d9b9622c87971e3f067b4926
Craig Blain <craig.blain@gmail.com>
2013-07-21 22:18:08 -0400

    initial commit

---
+++ ABA.m
@@ -0,0 +1,48 @@
+function kays = ABA(title, k0, kstep, kf, time_points, integrations)
+%Fit k values to multiple steps in a primer extension reaction
+%Takes a matrix of gel integrations (increasing time left to right,
+%increasing extension length bottom to top)
+%Fits a k value to each extension step one at a time
+
+times = time_points - time_points(1); %t0 now 0
+
+flipped_integrations = flipud(integrations); %primer now at top
+
+%for each product length n, add all integrations for >=n+1 into n+1 band
+%then fit a kay value for n->n+1
+kays=0;
+[num_bands,num_timepoints] = size(flipped_integrations);
+for b=2:num_bands
+    reduced_integrations = [flipped_integrations(1:b-1,:); sum(flipped_integrations(b:end,:),1)];
+    kays(b-1) = ABA_kScan(k0, kstep, kf, times, reduced_integrations, kays);
+end
+
+%Model fitted k values
+initial_integrations = reduced_integrations(:,1);
+model_output = initial_integrations;
+for t=2:length(times)
+    model_result = ABA_RateODE(kays(end),times(t),initial_integrations,kays(1:end-1));
+    [model_rows,model_columns] = size(model_result);
+    model_output(:,t) = model_result(model_rows,:)';
+end
+
+%Plot model output
+model_output = flipud(model_output);
+model_heatmap_data = flipud(fliplr((model_output)));
+y_labels = 0:length(kays);
+hm1 = HeatMap(model_heatmap_data,'RowLabels',y_labels,'ColumnLabels',time_points,'Symmetric',false,'DisplayRange',1,'Colormap',colormap(flipud(gray)),'Standardize','none');
+addXLabel(hm1,'Time (h)');
+addYLabel(hm1,'Extension');
+addTitle(hm1,strcat(title,' Model'));
+plot(hm1,figure(1))
+
+%Plot input data
+input_heatmap_data = flipud(fliplr((integrations)));
+y_labels = 0:length(kays);
+hm2 = HeatMap(input_heatmap_data,'RowLabels',y_labels,'ColumnLabels',time_points,'Symmetric',false,'DisplayRange',1,'Colormap',colormap(flipud(gray)),'Standardize','none');
+addXLabel(hm2,'Time (h)');
+addYLabel(hm2,'Extension');
+addTitle(hm2,strcat(title,' Data'));
+plot(hm2,figure(2))
+end
+
---
+++ ABA_kScan.m
@@ -0,0 +1,24 @@
+
+function kmin = ABA_kScan(k0, kstep, kf, time_points, integrations,kays)
+%scans a range of k values for a particular step in the primer extension
+%reaction and selections the one that gives the least squared error
+%from the measured integrations
+
+k_scan = k0:kstep:kf;
+r = zeros(length(k_scan));
+
+initial_integrations = integrations(:,1);
+for i=1:length(k_scan)
+    test_k=k_scan(i);
+    for j=2:length(time_points)
+        model = ABA_RateODE(test_k,time_points(j),initial_integrations, kays);
+        for k=1:size(model,2)
+            r(i)=r(i)+(integrations(k,j)-model(end,k))^2;
+        end
+    end
+end
+
+[rminn, index] = min(r);
+kmin = k_scan(index(1));
+
+end
